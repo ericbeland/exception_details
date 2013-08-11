489062b722c14ab96ff6e30552de11d4383c281d
Eric Beland <ebeland@gmail.com>
2013-08-11 10:55:10 -0400

    Stray tab removed

--- lib/exception_details/inspect_variables.rb
+++ lib/exception_details/inspect_variables.rb
@@ -22,7 +22,8 @@ module ExceptionDetails
 					if self.class.filter_variables.include?(vname.to_s)
 						value = '**FILTERED**'
 					end
-						variable_value_string = "\t" + sprintf("%-35s = %s", "<#{value.class}> #{vname}", value.inspect)
+					
+					variable_value_string = "\t" + sprintf("%-35s = %s", "<#{value.class}> #{vname}", value.inspect)
 
 					unless value == self # don't list the exception itself ...
 						variable_strings << variable_value_string
